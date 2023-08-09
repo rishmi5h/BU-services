a35f8e15b6a734426e7fd66dc9e111a775253d11
RISHABH MISHRA <rish.mish.2607@gmail.com>
2023-08-09 13:33:06 +0530

    update : id names

--- src/main/java/org/rishmi5h/model/Product.java
+++ src/main/java/org/rishmi5h/model/Product.java
@@ -12,6 +12,7 @@ public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
     private long productId;
 
     private String name;
